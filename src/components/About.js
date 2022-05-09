d9461fdd91b0a8272d024d8781866dd35958e863
samwel-chege <samwelchegeh09@gmail.com>
2022-05-09 17:37:25 +0300

    add an about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,31 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div></div>
+    <div className=''>
+      <h2 className='text-center text-3xl font-bold'>About us</h2>
+      <h3 className='text-3xl font-mono text-justify'>What we do is all about <a className='underline decoration-sky-500/30'>you</a> and <a className='underline decoration-pink-500/30'>your</a> <a className='underline decoration-indigo-500/30'>family business</a> </h3>
+      <div className='grid gap-4 grid-cols-3 grid-rows-3'>
+        <div className='col-lg-4'>
+          <a href="#" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          </a>
+        </div>
+        <div className='col-lg-4'>
+          <a href="#" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          </a>
+        </div>
+        <div className='col-lg-4'>
+          <a href="#" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          </a>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
