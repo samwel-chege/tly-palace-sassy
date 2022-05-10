9ba99505770872d5128149cfea7682540a7756c1
samwel-chege <samwelchegeh09@gmail.com>
2022-05-10 12:39:22 +0300

    mission

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Mission = () => {
   return (
-    <div></div>
+    <div>
+      
+        <a href="#" class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://unsplash.com/photos/oqStl2L5oxI" alt="img" />
+          <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Mission</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          </div>
+        </a>
+
+    </div>
   )
 }
 
