fe76a42d87996afb2c6d0aea95a4cd4d53ca14c3
samwel-chege <samwelchegeh09@gmail.com>
2022-05-19 21:22:03 +0300

    configure color customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'orange': '#f97316',
+  
+      },
+    },
   },
   plugins: [],
 }
