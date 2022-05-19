761474989ff7d9ddfb3d73bb53fd81fd3d3535a2
samwel-chege <samwelchegeh09@gmail.com>
2022-05-19 21:25:29 +0300

    customize the landing page

--- src/components/Header.js
+++ src/components/Header.js
@@ -47,10 +47,10 @@ const Header = () => {
             </header>
             <div class="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
               <div class="lg:w-3/5 xl:w-2/5 flex flex-col items-start relative z-10">
-                <span class="font-bold uppercase text-yellow-400 underline decoration-indigo-500">
+                <span class="font-bold uppercase text-cyan-500 underline decoration-indigo-500">
                   Sassy
                 </span>
-                <h1 class="font-bold text-6xl sm:text-7xl text-white leading-tight mt-4">
+                <h1 class="font-bold text-6xl sm:text-7xl text-amber-600 leading-tight mt-4">
                   Tally Palace Family
                   <br />
                   Office
