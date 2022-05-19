511150a4cadd4b4cc49f7aabf5acb15d5851dca4
samwel-chege <samwelchegeh09@gmail.com>
2022-05-19 22:52:34 +0300

    add some branding values

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -2,7 +2,106 @@ import React from 'react'
 
 const Brand = () => {
   return (
-    <div></div>
+    <div>
+      
+        <div class="container mx-auto px-6 p-6 bg-white dark:bg-gray-800 ">
+          <div class="mb-16 text-center">
+            <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+              Services
+            </h2>
+            <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+              <a className='underline decoration-sky-500'>You</a> <a className='underline decoration-indigo-500'>deserve</a> a <a className='underline decoration-pink-500'>dialed-in system</a>
+            </p>
+          </div>
+          <div class="flex flex-wrap my-12 dark:text-white">
+            <div class="w-full border-b md:w-1/2 md:border-r lg:w-1/3 p-8">
+              <div class="flex items-center mb-6">
+                <svg width="20" height="20" fill="currentColor" class="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                  </path>
+                </svg>
+                <div className="ml-4 text-xl">
+                  <a>Increase sales</a>
+                </div>
+              </div>
+              <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
+                Are you ready to take you customer relationships to the next level? You will be provided with hands-on training & workflow optimazations that will ensure your success
+              </p>
+            </div>
+            <div class="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r p-8">
+              <div class="flex items-center mb-6">
+                <svg width="20" height="20" fill="currentColor" class="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                  </path>
+                </svg>
+                <div class="ml-4 text-xl">
+                  Overlays
+                </div>
+              </div>
+              <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
+                You can only tell how strong a chain is by its weakest link. Thesame is true for your business & its process. If one process is slow, it will have an effect all the way down the chain
+              </p>
+            </div>
+            <div class="w-full border-b md:w-1/2 md:border-r lg:w-1/3 lg:border-r-0 p-8">
+              <div class="flex items-center mb-6">
+                <svg width="20" height="20" fill="currentColor" class="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                  </path>
+                </svg>
+                <div class="ml-4 text-xl">
+                  Control
+                </div>
+              </div>
+              <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
+               To be succesful, a business must manage multiple tasks simultaneously. Among these many tasksis accounting, which delas with records, business reports,etc
+              </p>
+            </div>
+            <div class="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r lg:border-b-0 p-8">
+              <div class="flex items-center mb-6">
+                <svg width="20" height="20" fill="currentColor" class="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                  </path>
+                </svg>
+                <div class="ml-4 text-xl">
+                  Mapping
+                </div>
+              </div>
+              <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
+                In many industries, project management software is used to plan projects, schedule resources and allocate costs. PMS ensures data security
+              </p>
+            </div>
+            <div class="w-full border-b md:w-1/2 md:border-r md:border-b-0 lg:w-1/3 lg:border-b-0 p-8">
+              <div class="flex items-center mb-6">
+                <svg width="20" height="20" fill="currentColor" class="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                  </path>
+                </svg>
+                <div class="ml-4 text-xl">
+                  Fill the missing
+                </div>
+              </div>
+              <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
+                By optimizing a workflow, you can reduce operating costs, improve efficiency, add new features to an existing workflow, reduce the time it takes to complete a task, and many other factors thst can improve efficiency & effectiveness
+              </p>
+            </div>
+            <div class="w-full md:w-1/2 lg:w-1/3 p-8">
+              <div class="flex items-center mb-6">
+                <svg width="20" height="20" fill="currentColor" class="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                  </path>
+                </svg>
+                <div class="ml-4 text-xl">
+                  Dynamic Texts
+                </div>
+              </div>
+              <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
+                Increasing demand leads to increased workload & therefore a business is unable to hsndle thngs manually. Automated accounting software has swayed business a new direction
+              </p>
+            </div>
+          </div>
+        </div>
+
+    </div>
   )
 }
 
