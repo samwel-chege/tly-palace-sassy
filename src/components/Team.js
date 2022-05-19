2dd4782385cb8f1040fa4f7aaec0375376a818d5
samwel-chege <samwelchegeh09@gmail.com>
2022-05-19 21:19:35 +0300

    create a Team component

---
+++ src/components/Team.js
@@ -0,0 +1,79 @@
+import React from 'react'
+
+const Team = () => {
+  return (
+    <div>
+        
+                <div class="p-8 bg-white dark:bg-gray-800 rounded-lg shadow bg-slate-100">
+                    <p class="text-center text-3xl font-bold text-amber-600 dark:text-white">
+                        Professional team
+                    </p>
+                    <p class="text-center mb-12 text-xl font-normal text-cyan-500 dark:text-gray-200">
+                        Meet the best team in world
+                    </p>
+                    <div class="flex items-center flex-col md:flex-row justify evenly">
+                        <div class="p-4">
+                            <div class="text-center mb-4 opacity-90">
+                                <a href="#" class="block relative">
+                                    <img alt="profil" src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="mx-auto object-cover rounded-full h-40 w-40 " />
+                                </a>
+                            </div>
+                            <div class="text-center">
+                                <p class="text-2xl text-amber-500 dark:text-white">
+                                    Maureen Wanjiru
+                                </p>
+                                <p class="text-xl text-gray-500 dark:text-gray-200 font-light">
+                                    CEO & Systems Strategist
+                                </p>
+                                <p class="text-md text-gray-500 dark:text-gray-400 max-w-xs py-4 font-light">
+                                   An accountant by training, an autodidact Business Systems Strategist
+                                </p>
+                            </div>
+      
+                        </div>
+                        <div class="p-4">
+                            <div class="text-center mb-4 opacity-90">
+                                <a href="#" class="block relative">
+                                    <img alt="profil" src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="mx-auto object-cover rounded-full h-40 w-40 " />
+                                </a>
+                            </div>
+                            <div class="text-center">
+                                <p class="text-2xl text-amber-500 dark:text-white">
+                                    Antony Munyau
+                                </p>
+                                <p class="text-xl text-gray-500 dark:text-gray-200 font-light">
+                                    Head of information Technology
+                                </p>
+                                <p class="text-md text-gray-500 dark:text-gray-400 max-w-xs py-4 font-light">
+                                    Versatile and personable information scientist with 6+ yrs of experience solving problems, building and maintaining data environments.Adept with process mapping to create amazing workflows that work as the backbone of your business
+                                </p>
+                            </div>
+                           
+                        </div>
+                        <div class="p-4">
+                            <div class="text-center mb-4 opacity-90">
+                                <a href="#" class="block relative">
+                                    <img alt="profil" src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" class="mx-auto object-cover rounded-full h-40 w-40 " />
+                                </a>
+                            </div>
+                            <div class="text-center">
+                                <p class="text-2xl text-amber-500 dark:text-white">
+                                    Kevin Omaanya
+                                </p>
+                                <p class="text-xl text-blue-500 dark:text-gray-200 font-light">
+                                    Head of marketing
+                                </p>
+                                <p class="text-md text-gray-500 dark:text-gray-400 max-w-xs py-4 font-light">
+                                    Experienced data analyst with an interest in big data
+                                </p>
+                            </div>
+                          
+                        </div>
+                    </div>
+                </div>
+
+    </div>
+  )
+}
+
+export default Team
